4085bfab4af7d2a46a8f5f4e1e1879d402798258
Jonas Aldén <jonas.alden@dormy.se>
2020-04-14 00:20:05 +0200

    Loadingscreen ajax

--- Landlord_project/wwwroot/js/site.js
+++ Landlord_project/wwwroot/js/site.js
@@ -1,1 +1,8 @@
-﻿
+﻿function LoadingScreen(show) {
+    if (show === true) {
+        $('body').append('<div id="landlord-backdrop"><img id="landlord-backdrop-spinner" src="~/wwwroot/images/spinner.gif" /></div>');
+    }
+    else {
+        $('#landlord-backdrop').remove();
+    }
+}
